518a7ce34cc80506e84cccf83c2113b66bde44de
Babavaraprasad <bapi@itu.dk>
2022-08-18 12:29:56 +0200

    HTML,css changes

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Todo from './components/Todo';
+
+function App() {
+  return (
+<div>    
+      <div className='h1'><h1>MY TODO BABAVARAPRASAD</h1></div>
+      <Todo text='learn react'></Todo>
+</div>
+  );
+}
+
+export default App;
---
+++ src/components/Todo.js
@@ -0,0 +1,12 @@
+
+function Todo(){
+return(
+    <div className='Card'>
+    <h2>TITLE</h2>
+    <div className='button'>
+    <button>Delete</button></div>
+    </div>
+)
+}
+
+export default Todo;
